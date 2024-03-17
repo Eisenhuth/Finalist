8b20ed67a37503b74d4f11a09a62b5aa352ba321
Jan Eisenhuth <47415874+Eisenhuth@users.noreply.github.com>
2024-03-17 21:50:39 +0100

    fixed status bar theme

--- Finalist/FinalistApp.swift
+++ Finalist/FinalistApp.swift
@@ -5,7 +5,7 @@ struct FinalistApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .preferredColorScheme(.light)
+                .preferredColorScheme(.dark)
         }
     }
 }
