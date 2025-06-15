91392cfdd7e8bdff7df47b5cbb27106b0dd4ee25
Jan Eisenhuth <47415874+Eisenhuth@users.noreply.github.com>
2025-06-15 18:42:26 +0200

    added highlighting

--- Finalist/Leaderboard/PlayerName.swift
+++ Finalist/Leaderboard/PlayerName.swift
@@ -1,6 +1,9 @@
 import SwiftUI
 
 struct PlayerName: View {
+    @AppStorage("highlightName") private var highlightName: String = "Oscar#1234"
+    @AppStorage("highlightClub") private var highlightClub: String = "EMBRK"
+    
     var name: String
     var clubTag: String?
     
@@ -15,32 +18,41 @@ struct PlayerName: View {
                     Text(clubTag)
                         .font(.finalsBody(10))
                         .frame(width: 35)
-                        .background(Color.gray.opacity(0.2))
+                        .background(clubTag == highlightClub ? .finalsYellow.opacity(0.4) : Color.gray.opacity(0.2))
                         .clipShape(RoundedRectangle(cornerRadius: 2))
                         .overlay(
                             RoundedRectangle(cornerRadius: 2)
                                 .stroke(Color.gray.opacity(0.4), lineWidth: 1)
                         )
+                        .foregroundStyle(clubTag == highlightClub ? .finalsYellow : .finalsWhite)
                 } else {
                     Text("")
                         .frame(width: 35)
                 }
             }
             
-            Text(name).font(.finalsBodyEmphasis())+Text("#\(number)").foregroundStyle(.secondary)
+            Text(name)
+                .foregroundStyle(self.name == highlightName ? .finalsYellow : .finalsWhite)
+                .font(.finalsBodyEmphasis())+Text("#\(number)").foregroundStyle(.secondary)
                 .font(.finalsBody(12))
             
             Spacer()
         }
-        .font(.finalsBody(16))
         .lineLimit(1)
     }
 }
 
 #Preview {
-    PlayerName(name: "Oscar#1234", clubTag: "ABCDE")
-    PlayerName(name: "Oscar#1234", clubTag: "")
-    PlayerName(name: "Oscar#1234", clubTag: "ABC")
-
+    ZStack {
+        Color.finalsRed
+            .ignoresSafeArea()
+        
+        VStack {
+            PlayerName(name: "Oscar#1234", clubTag: "ABCDE")
+            PlayerName(name: "Matt#1234", clubTag: "EMBRK")
+            PlayerName(name: "Peter#1234", clubTag: "")
+            PlayerName(name: "Oscar#1234", clubTag: "ABC")
+        }
+    }
 
 }
